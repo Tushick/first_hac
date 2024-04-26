73e70fbfbfbac5a6ddea27b0bdfd8c063017de08
ZTushik <nmks_4444@mail.ru>
2024-04-26 18:41:29 +0300

    baza

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
+import os
+
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+os.system("cls")
+
 import pandas as pd
 import numpy as np
+
+from nltk import MWETokenizer
 from config import *
 
-data_frame = pd.read_csv(TRAIN_DATA_FILE)
-train_data = data_frame[["Question", "answer_class"]]
+# ? Import ml
+import keras
+from keras.layers import *
+
+#? Workers
+tokenizer = MWETokenizer()
+
+data_frame_train = pd.read_csv(TRAIN_DATA_FILE)
+train_data = data_frame_train[["Question", "answer_class"]]
 
 print(train_data)
+
+input_data = []
+output_data = []
+for string in train_data["Question"]:
+    data = tokenizer.tokenize(string)
+    input_data.append(data)
+for class_str in train_data["answer_class"]:
+    output_data.append(class_str)
+
+
+print(input_data)
+
+
+# input_data = np.array()
+# output_data = np.array()
+
+
+
+
+
+
